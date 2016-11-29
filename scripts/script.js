513cf56c8f4f06f64afa241adc10236b148db596
Ethan Wright <ethan.wright@gmail.com>
2016-11-29 15:02:13 +1100

    rainbow hover working

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
-
+var roygbiv = 0;
+function rainbowHover() {
+		roygbiv++;
+		if (roygbiv == 7) {
+			roygbiv = 0
+		}
+	}
 
 $(document).ready(function() {
 	var skBox = '<div class="sketch"></div>';
@@ -11,10 +17,28 @@ $(document).ready(function() {
 	};
 
 	
-	
+	//hover : black
+	/*
 	$('.sketch').mouseenter(function() {
 		$(this).css("background-color", "black");
 	});
+	*/
+	
+		// hover : rainbow 
+	
+	var rainbowColour = ["#e40013", "#ff5f3c", "#fcf651", "#00bc38", "#009df8", "#08077b", "#320993" ]
+	
+	
+		
+	$('.sketch').mouseenter(function() {
+		rainbowHover();
+		$(this).css("background-color", rainbowColour[roygbiv]);
+	});
+	
+	
+	
+	
+	
 });
 
 
