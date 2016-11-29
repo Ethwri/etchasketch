d3e2010e1c48100789fcb3d0d23173893eb0450c
Ethan Wright <ethan.wright@gmail.com>
2016-11-29 13:33:41 +1100

    basic 16x16 grid working

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,16 @@
 
 $(document).ready(function() {
 	var skBox = '<div class="sketch"></div>';
-	$('#canvas').append(skBox);
-	$('#canvas').append(skBox);
-	$('#canvas').append(skBox);
-	$('#canvas').append(skBox);
-	$('#canvas').append(skBox);
+	for(var gridRow=0; gridRow<16; gridRow++) {
+		$('#canvas').append('<div class = "row">');
+		for(var gridWidth=0; gridWidth<16; gridWidth++) {
+			$('.row').last().append(skBox);
+		};
+		$('#canvas').append('</div>');
+	};
+
+	
+	
 	$('.sketch').mouseenter(function() {
 		$(this).css("background-color", "black");
 	});
