3a049cefa83ba3048324a2269a6fda21e5851068
Ethan Wright <ethan.wright@gmail.com>
2016-11-29 17:22:05 +1100

    random colour iteration #1

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,24 @@ function rainbowHover() {
 		}
 	}
 
+	
+	
+var randColour = new Array();
+function randHex() {
+	var randTemp = 0 ;
+	 // =[hex1, hex2, hex3, hex4, hex5, hex6 ]
+	var hexCode = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c" ,"d", "e", "f" ]
+	var count = 6;
+	while(count >= 0) {
+		randTemp = Math.floor(Math.random() * 15) + 0 ;
+		randColour.push(hexCode[randTemp]);
+		count--;
+	}	
+    return randColour[0] + randColour[1] +randColour[2] + randColour[3] + randColour[4] + randColour[5];
+}	
+
+	
+
 $(document).ready(function() {
 	var skBox = '<div class="sketch"></div>';
 	for(var gridRow=0; gridRow<16; gridRow++) {
@@ -24,21 +42,25 @@ $(document).ready(function() {
 	});
 	*/
 	
-		// hover : rainbow 
 	
+	// hover : rainbow 
+	/*
 	var rainbowColour = ["#e40013", "#ff5f3c", "#fcf651", "#00bc38", "#009df8", "#08077b", "#320993" ]
-	
-	
 		
 	$('.sketch').mouseenter(function() {
 		rainbowHover();
 		$(this).css("background-color", rainbowColour[roygbiv]);
 	});
+	*/
 	
 	
+		
+	$('.sketch').mouseenter(function() {
+		randHex();
+		$(this).css("background-color", "#" + randColour[0] + randColour[1] +randColour[2] + randColour[3] + randColour[4] + randColour[5]);
+		randColour = [];
 	
-	
-	
+	});
 });
 
 
